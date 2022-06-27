f07717c32c44638b94455801bf5d61757c39eba9
jstpd <jstpd228@gmail.com>
2022-06-27 19:55:42 +0300

    a i'm sorry...

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Logger from './helpers/logger'
 import Hash from './helpers/hash'
 import Validator from './helpers/validator'
+import Cookie from './helpers/cookie'
 
 export {
   Logger,
   Hash,
-  Validator
+  Validator,
+  Cookie
 }
